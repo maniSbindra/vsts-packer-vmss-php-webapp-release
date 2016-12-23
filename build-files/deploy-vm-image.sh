26f78959867eede6e965d58de85b9e320c3c3acb
manisbindra <maninder.bindra@gmail.com>
2016-12-23 11:57:11 +0530

    added shebang

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 cat azuredeploy.parameters.json
 azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json  -g $1 -n rocdashdeploy$2
--- build-files/ui-tests/execute-site-smoke-tests.sh
+++ build-files/ui-tests/execute-site-smoke-tests.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 phpunit $1site-smoke-test.php $2
